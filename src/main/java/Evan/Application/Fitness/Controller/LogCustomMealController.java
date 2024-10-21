8f7bb060533d20d819053ba83708a50ea858ed44
evan <evan.lindsay223@gmail.com>
2024-10-20 21:25:08 -0400

    adding Custom meal controller

--- src/main/java/Evan/Application/Fitness/Controller/LogCustomMealController.java
+++ src/main/java/Evan/Application/Fitness/Controller/LogCustomMealController.java
@@ -1,7 +1,16 @@
 package Evan.Application.Fitness.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LogCustomMealController {
+    @Autowired
+
+    @GetMapping("/add/custom/meal")
+    private String addCustomMealOption(Model model){
+        model.addAttribute()
+    }
 }
