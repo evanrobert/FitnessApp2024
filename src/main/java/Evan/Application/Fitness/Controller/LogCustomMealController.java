bcd34af9179cec6afce101ef7d5c2ccde749e5b1
evan <evan.lindsay223@gmail.com>
2024-10-20 21:30:11 -0400

    adding getmapping in Calorie Controller

--- src/main/java/Evan/Application/Fitness/Controller/LogCustomMealController.java
+++ src/main/java/Evan/Application/Fitness/Controller/LogCustomMealController.java
@@ -1,5 +1,7 @@
 package Evan.Application.Fitness.Controller;
 
+import Evan.Application.Fitness.Model.CalorieInformation;
+import Evan.Application.Fitness.Repositorys.CalorieInformationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,9 +10,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LogCustomMealController {
     @Autowired
-
+    CalorieInformationRepository calorieInformationRepository;
     @GetMapping("/add/custom/meal")
     private String addCustomMealOption(Model model){
-        model.addAttribute()
+        model.addAttribute("calorieInformation",new CalorieInformation());
+        return "customMeal";
     }
 }
