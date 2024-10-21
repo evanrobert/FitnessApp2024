82575ee2395353cab39d3e727cd5a2f3d36ed4c4
evan <evan.lindsay223@gmail.com>
2024-10-20 21:22:53 -0400

    adding a custom meal option

---
+++ src/main/java/Evan/Application/Fitness/Controller/LogCustomMealController.java
@@ -0,0 +1,7 @@
+package Evan.Application.Fitness.Controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class LogCustomMealController {
+}
---
+++ src/main/java/Evan/Application/Fitness/Model/CalorieInformation.java
@@ -0,0 +1,37 @@
+package Evan.Application.Fitness.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+public class CalorieInformation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+private String itemName;
+    private double calories;
+    private double proteins;
+    private double fats;
+    private double carbohydrates;
+    private double fiber;
+    private double sugars;
+    private double sodium;
+    private double cholesterol;
+    private String breakfast;
+    private String lunch;
+    private String dinner;
+    private Date date;
+    @OneToOne
+    @JoinColumn(name = "userid")
+    private UserLoginDetails userLoginDetails;
+
+}
