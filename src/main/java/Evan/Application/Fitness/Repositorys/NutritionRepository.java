ef9d8fc49e7ed481d424e992382817b586426def
evan <evan.lindsay223@gmail.com>
2024-11-22 15:06:35 -0500

    removing unneeded nutrition class and repo

--- src/main/java/Evan/Application/Fitness/Repositorys/NutritionRepository.java
+++
@@ -1,9 +0,0 @@
-package Evan.Application.Fitness.Repositorys;
-
-import Evan.Application.Fitness.Model.Nutrition;
-import Evan.Application.Fitness.Model.UserLoginDetails;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NutritionRepository extends JpaRepository<Nutrition, Long> {
-
-}
